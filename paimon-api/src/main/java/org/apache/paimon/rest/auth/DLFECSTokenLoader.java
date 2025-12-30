744a813fa2a8eab5678edff61fb84d53e4e8deb5
mj375349 <mj375349@alibaba-inc.com>
2025-12-30 11:27:15 +0800

    [core] Simplify exception handling and preserve runtime exceptions

--- paimon-api/src/main/java/org/apache/paimon/rest/auth/DLFECSTokenLoader.java
+++ paimon-api/src/main/java/org/apache/paimon/rest/auth/DLFECSTokenLoader.java
@@ -88,6 +88,8 @@ public class DLFECSTokenLoader implements DLFTokenLoader {
                         System.currentTimeMillis() - startTime);
             }
             return responseBodyStr;
+        } catch (RuntimeException e) {
+            throw e;
         } catch (Exception e) {
             throw new RuntimeException("get response failed, error : " + e.getMessage(), e);
         }
